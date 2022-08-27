cf59b1ea627b67224c91f9c564acdf6bd085e1d1
aaeickem <aaeickem@ncsu.edu>
2022-08-27 14:04:30 -0400

    fix improper file name

--- nvd_api_connector.py
+++ nvd_api_connector.py
@@ -21,7 +21,18 @@ class NVDAPIConnector:
         cveID (str) -- CVE ID to return information on
         """
 
-        return None
+        query_type = "cve"
+        api_version = "1.0"
+
+        if self.api_key is None:
+            params = None
+        else:
+            params = {"apiKey": self.api_key}
+
+        query_url = "/".join(self.api_url, query_type, api_version, cveID)
+        response = requests.get(url = query_url, params = params)
+
+        return response
 
     def get_cves(self, startIndex: int) -> None: # Dev Note: Change return type
         """Return information on multiple CVEs in JSON format.
