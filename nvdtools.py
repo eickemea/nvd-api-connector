2cdb4f21f0cb60317df76ef475520cbbf037d84f
aaeickem <aaeickem@ncsu.edu>
2023-02-25 15:39:30 -0500

    Added base method to send GET request to CVE API

--- nvdtools.py
+++ nvdtools.py
@@ -29,7 +29,7 @@ class NVDConnector:
 
 
 class CVEConnector(NVDConnector):
-    """Class containing methods to query the National Vulnerability Database V2 CVE API."""
+    """Class containing methods to query the National Vulnerability Database v2 CVE API."""
 
     def __init__(self, api_key: Optional[str] = None) -> None:
         """Initialization method for class.
@@ -39,5 +39,29 @@ class CVEConnector(NVDConnector):
         """
 
         super().__init__(api_key)
+        self.url = self.create_api_url('cves/2.0')
+
+
+    def get_cves(self, cveId: Optional[str] = None, resultsPerPage: int = 2000, startIndex = 0) -> requests.Response:
+        """Send GET request to National Vulnerability Database v2 CVE API.
+        
+        Keyword arguments:
+        cveId -- CVE ID to query for.
+        resultsPerPage -- Number of records returned by API. Maximum allowable value is 2000.
+        startIndex -- Index of the first CVE to be returned in the response. Zero-indexed.
+        """
+
+        # Send GET request
+        headers = {
+            'apiKey': self.api_key
+        }
+        payload = {
+            'cveId': cveId,
+            'resultsPerPage': resultsPerPage,
+            'startIndex': startIndex
+        }
+        response = requests.get(self.url, params = payload, headers = headers)
+
+        return response
 
     
